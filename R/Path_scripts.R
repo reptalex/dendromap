65203415e4edf2ec4235fd76e78f37a9fbbca0d8
sfodel <sfodel@users.noreply.github.com>
2019-03-13 10:17:34 +0100

    Update Path_scripts.R

--- R/Path_scripts.R
+++ R/Path_scripts.R
@@ -1,5 +1,5 @@
 ##Here I give three functions that return:
-## 1) all the non-terminal paths starting from the root
+## 1) all the non-terminal paths starting from one node after the root
 ## 2) all the depth intervals of these paths
 ## 3) the orientation of all those paths
 ## The outputs of all three functions are lists, and the element numbers in these lists correspond to the same path 
@@ -9,12 +9,14 @@
 library(ape)
 library(phangorn)
 
+#Updated this to start one node after the root, we do not need the root
 trimpaths <- function(Tree) {
   Nodepath <- nodepath(Tree, from=Ntip(tree)+1)
   trimedpath <- list(length=length(Nodepath))
   for (i in 1:(length(Nodepath))) {
     trimedpath[[i]] <- Nodepath[[i]][-length(Nodepath[[i]])]
-  }
+    trimedpath[[i]] <- trimedpath[[i]][-1]
+    }
   trimedpath <- unique(trimedpath)
   return(trimedpath)
 }
@@ -33,24 +35,27 @@ pathdepths <- function(trimedpath, tree) {
 }
 
 #Here again trimedpath stands for the output of trimpaths, not the output of pathdepths
+#Updated the script to give the correct orientation no matter the number of tips of the tree
 pathorient <- function(trimedpath, tree) {
   Desc <- Descendants(tree, (Ntip(tree)+1), type="all")
   desc <- vector(length = length(Desc))
   is.even <- function(x) x %% 2 == 0
+
   for (i in 1:(length(Desc))) {
     if (isTRUE(is.even(i))) {
-      desc[i] <- 'a'
-    } else {
       desc[i] <- 'b'
+    } else {
+      desc[i] <- 'a'
     }
   }
   
   for (j in 1:(length(trimedpath))) {
     for (i in 1:length(trimedpath[[j]])) {
-      for (k in (Ntip(tree)+1):length(desc)) {
-        trimedpath[[j]] <- replace(trimedpath[[j]], trimedpath[[j]]==k, desc[k])
+      for (k in 1:length(Desc)) {
+        trimedpath[[j]] <- replace(trimedpath[[j]], trimedpath[[j]]==Desc[k], desc[k])
       }
     }
   }
+  
   return(trimedpath)
 }
