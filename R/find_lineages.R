f825dc8a0403dfc0cbf48830f8a348339d31050a
Alex Washburne <bigalculus@gmail.com>
2019-09-20 10:29:43 -0600

    dendromap!!!

--- R/find_lineages.R
+++ R/find_lineages.R
@@ -9,18 +9,24 @@ find_lineages <- function(RCmap,rc_table,
                           col.nodemap){
   ### will start with index as descendant and traverse up
   ix <- which(RCmap$terminal)
-  base::cat('\nThere are',length(ix),'terminal nodes. Saddle up and find the trunk, ants!')
+  base::cat('\nThere are',length(ix),'terminal nodes. Traversing RC tree from all terminal nodes. ')
+  
+  ### parallelizable
   Seqs <- lapply(ix,rc_seqs,RCmap) %>% unlist(recursive=FALSE) %>% unique
-  base::cat('\nFound',length(Seqs),'RC sequences')
+  
+  
   n <- length(Seqs)
-  
-  
   tbl <- data.table('seq1'=rep(1:(n-1),times=(n-1):1),key='seq1')
   tbl[,seq2:=(seq1+1):n,by=seq1]
+  base::cat(paste('\nFound',length(Seqs),'RC sequences for',nrow(tbl),'pairs. Checking joinability of pairs.'))
   
+  
+  ### parallelizable, and possibly much more efficient if vectorized w/ data.table
   joinability <- apply(t(tbl),2,FUN=function(x,s,rc,r,c) check_joinable(x[1],x[2],s,rc,r,c),
                        s=Seqs,rc=rc_table,r=row.nodemap,c=col.nodemap)
   tbl[,joinability:=joinability]
+  
+  
   ### we recursively group joinable sequences into unique index sets...
   ### is joinability transitive? No.
   ### However, it is true that the union of joinable sequences will dominate either sequence
@@ -29,17 +35,31 @@ find_lineages <- function(RCmap,rc_table,
   ### We can find these with the function igraph::cliques
   joinables <- tbl[joinability==TRUE]
   nedge <- nrow(joinables)
-  nvert <- length(unique(unlist(joinables[,c('seq1','seq2')])))
-  base::cat(paste('\nFound',nedge,'joinable RC sequences \ncontaining',nvert,' joinable pairs. \n...how hard is it to find cliques in a graph of',nvert,'vertices and',nedge,'edges?'))
+  seq.indexes <- unique(unlist(tbl[,c('seq1','seq2')]))
+  nverts <- length(seq.indexes)
+  VertMap <- data.table('seq'=seq.indexes,'vert'=paste('v',seq.indexes,sep='_'),key='vert')
+  
+  base::cat(paste('\nFound',nedge,'joinable RC sequences \ncontaining',nverts,' joinable pairs. \n...how hard is it to find cliques in a graph of',nverts,'vertices and',nedge,'edges?'))
   joinable.edges <- split(joinables[,c('seq1','seq2')],seq(nrow(joinables))) %>% unlist
+  joinable.edges <- VertMap[match(joinable.edges,seq),vert]
   
   G <- igraph::make_graph(joinable.edges,directed = F)
   
   base::cat('\nMaking cliques')
-  joinable.seqs <- igraph::cliques(G) ### now we need to remove elements that are strict subsets of other sets
+  
+  ########## FIND MAXIMAL CLIQUES
+  ### below can be parallelized by defining subsets (throw out disconnected nodes)
+  joinable.seqs <- igraph::cliques(G,min=2) ### now we need to remove elements that are strict subsets of other sets
   joinable.seqs <- joinable.seqs[order(sapply(joinable.seqs,length),decreasing = F)]
-  base::cat(paste('\nFound joinable cliques of sizes',sapply(joinable.seqs,length)[1:5],'...'))
+  
+  
+  lns <- sapply(rev(joinable.seqs),length)
+  big.lns <- paste(lns[1:5],collapse=',')
+  small.lns <- paste(lns[(length(joinable.seqs)-4):length(joinable.seqs)],collapse=',')
+  base::cat(paste('\nFound joinable cliques of sizes',big.lns,'...',small.lns))
   base::cat('\nRemoving subsets to find maximal cliques')
+  
+  ### below can be parallelized
   for (i in 1:(length(joinable.seqs)-1)){
     check.subset <- any(sapply(joinable.seqs[(i+1):length(joinable.seqs)],
                                FUN=function(a,b) all(b %in% a),b=joinable.seqs[[i]]))
@@ -50,9 +70,15 @@ find_lineages <- function(RCmap,rc_table,
   found.cliques <- !sapply(joinable.seqs,FUN=function(x) all(is.na(x)))
   base::cat(paste('\nThere are',sum(found.cliques),'cliques remaining'))
   joinable.seqs <- joinable.seqs[found.cliques]
-  joinable.seqs <- c(setdiff(unique(unlist(tbl[,c(1,2)])),joinable.seqs),
-                     joinable.seqs)
+  
+  ## add disconnected RC's
+  all.RC.cliques <- c(as.list(setdiff(VertMap$vert,joinable.edges)),
+                     lapply(joinable.seqs,names))
+  
+  all.RC.cliques <- lapply(all.RC.cliques,FUN=function(v,VertMap) VertMap[match(v,vert),seq],
+                        VertMap)
+  
   get_rc_index <- function(x,Seqs.=Seqs) unique(unlist(Seqs[x]))
-  Seqs <- lapply(joinable.seqs,get_rc_index)
-  return(Seqs)
+  RCSeqs <- lapply(all.RC.cliques,get_rc_index)
+  return(RCSeqs)
 }
